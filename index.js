a4cacd3a3728b950940317a0afefbad703caa7fb
Arthur Ish <arthurishdev@gmail.com>
2024-04-28 15:33:01 -0700

    sums summed

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    for (let tester = 0; tester < array.length; tester++) {
+      for (let testee = tester + 1; testee < array.length; testee++) {
+        if (target === array[tester] + array[testee]) {
+          return true;
+        }
+      }
+    }
+    return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
+  quadratic
 */
 
 /* 
   Add your pseudocode here
+  I use flowcharts on a whiteboard and stickynotes
+  Nested iteration to check each num for sum
 */
 
 /*
   Add written explanation of your solution here
+  iterate through array checking each number with each other to check
+  for the sum and return true if found.
 */
 
 // You can run `node index.js` to view these console logs
